14e0a1df150c7611f2b5a49f85f535f877a87eea
MuhammadShariq05 <shariqwrk12023@gmail.com>
2024-09-16 15:20:08 +0530

    update and insert

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,67 @@
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
+
+async function insertUser(
+  username: string,
+  password: string,
+  firstname: string,
+  lastname: string,
+  email: string
+) {
+  const res = await prisma.user.create({
+    data: {
+      username,
+      password,
+      firstname,
+      lastname,
+      email,
+    },
+    select: {
+      id: true,
+      firstname: true,
+      lastname: true,
+    },
+  });
+  console.log(res);
+}
+
+insertUser(
+  "sammy2",
+  "Newpassword1",
+  "Muhammad",
+  "Shariq",
+  "shariq45565@gmail.com"
+);
+
+interface UpdateParams {
+  firstName: string;
+  lastName: string;
+}
+
+async function updateUser(
+  username: string,
+  { firstName, lastName }: UpdateParams
+) {
+  return await prisma.user.update({
+    where: { username },
+    data: {
+      firstname: firstName,
+      lastname: lastName,
+    },
+  });
+}
+
+updateUser("sammy", {
+  firstName: "Md",
+  lastName: "Aarib",
+})
+  .then(() => {
+    console.log("Updated");
+  })
+  .catch((err) => {
+    console.error(`Error: ${err}`);
+  })
+  .finally(() => {
+    prisma.$disconnect();
+  });
